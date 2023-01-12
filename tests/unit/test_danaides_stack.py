4b249bc1c8d9174fc90ac4184b3f7ff273884543
Max Copeland <maxcopeland88@gmail.com>
2023-01-12 12:57:45 -0700

    Initial commit

--- tests/unit/test_danaides_stack.py
+++ tests/unit/test_danaides_stack.py
@@ -4,10 +4,10 @@ import aws_cdk.assertions as assertions
 from danaides.danaides_stack import DanaidesStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in danaides_tmp/danaides_stack.py
+# resource in danaides/danaides_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = DanaidesStack(app, "danaides_tmp")
+    stack = DanaidesStack(app, "danaides")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
