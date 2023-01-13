800d686490b7b9e2371fc18d34adc0ee34dbc983
Max Copeland <maxcopeland88@gmail.com>
2023-01-13 16:21:13 -0700

    Add misc folder

--- danaides/danaides_stack.py
+++ danaides/danaides_stack.py
@@ -1,19 +1,40 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
 from constructs import Construct
+
+from cdk_dynamo_table_view import TableViewer
+from .hitcounter import HitCounter
+
 
 class DanaidesStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        hello = _lambda.Function(
+            self, "HelloHandler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="hello.handler"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "DanaidesQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=hello
+        )
+
+        my_apigw = apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
+        )
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+            sort_by='-hits',
+        )
+
---
+++ scripts/ssm_server.sh
@@ -0,0 +1,8 @@
+export AWS_REGION=us-east-1
+export BASTION_INSTANCE_ID=$(aws ec2 describe-instances \
+                             --region $AWS_REGION \
+                             --filter "Name=tag:Name,Values=BastionHost" \
+                             --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" \
+                             --output text \
+                             --profile dev)
+aws ssm start-session --target $BASTION_INSTANCE_ID --region=$AWS_REGION --profile dev
